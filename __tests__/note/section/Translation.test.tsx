38b70fc9b455094617cefbe13dadccae6a8af848
fasaas <aduriz_06@hotmail.com>
2020-12-26 10:53:38 +0100

    Cover translation

---
+++ __tests__/note/section/Translation.test.tsx
@@ -0,0 +1,47 @@
+import '@testing-library/jest-native/extend-expect'
+import React from 'react'
+import { fireEvent, render } from '@testing-library/react-native'
+import Note from '../../../src/note'
+
+describe('Translation section', () => {
+    test('Renders a from input', () => {
+        const { queryByPlaceholderText } = render(<Note sections={[ATranslationSection({ id: '1' })]} />)
+
+        expect(queryByPlaceholderText('Например ...'))
+    })
+
+    test('Renders a to input', () => {
+        const { queryByPlaceholderText } = render(<Note sections={[ATranslationSection({ id: '1' })]} />)
+
+        expect(queryByPlaceholderText('Por ejemplo ...'))
+    })
+
+    test('Updates the from input with the typed values', () => {
+        const { queryByPlaceholderText, queryByDisplayValue } = render(
+            <Note sections={[ATranslationSection({ id: '1' })]} />
+        )
+
+        fireEvent.changeText(queryByPlaceholderText('Например ...'), 'From input updated value')
+
+        expect(queryByDisplayValue('From input updated value')).toBeTruthy()
+    })
+
+    test('Updates the to input with the typed values', () => {
+        const { queryByPlaceholderText, queryByDisplayValue } = render(
+            <Note sections={[ATranslationSection({ id: '1' })]} />
+        )
+
+        fireEvent.changeText(queryByPlaceholderText('Por ejemplo ...'), 'To input updated value')
+
+        expect(queryByDisplayValue('To input updated value')).toBeTruthy()
+    })
+})
+
+const ATranslationSection = ({ id, from = '', to = '' }: { id: string; from?: string; to?: string }): ISection => {
+    return {
+        type: '@native/translation',
+        id,
+        name: 'Translation',
+        props: { from, to },
+    }
+}
