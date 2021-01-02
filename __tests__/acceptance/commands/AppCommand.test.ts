494a1dbe01442a3790eb18bcf260cdb555918d80
fasaas <aduriz_06@hotmail.com>
2021-01-02 15:02:36 +0100

    FIRE!

--- __tests__/acceptance/commands/AppCommand.test.ts
+++ __tests__/acceptance/commands/AppCommand.test.ts
@@ -1,9 +1,45 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import AppCommands from '../../../src/commands/AppCommands'
 
 describe('App Commands', () => {
     describe('Save', () => {
-        describe('Given AsyncStorage save fails', () => {
-            test('Return failed', async () => {})
+        describe('Return failed', () => {
+            test.each([null, undefined, '', ' ', '  '])('Given id is invalid', async (invalidId) => {
+                const result = await AppCommands.saveNote(invalidId)
+
+                expect(result.failed).toBeTruthy()
+            })
+
+            test.each([null, undefined, '', ' ', '  '])('Given title is invalid', async (invalidTitle) => {
+                const result = await AppCommands.saveNote('valid Id', { title: invalidTitle })
+
+                expect(result.failed).toBeTruthy()
+            })
+
+            test.each([null, undefined, '', '7'])('Given createdAt is not a number', async (invalidCreatedAt) => {
+                const result = await AppCommands.saveNote('valid Id', {
+                    title: 'valid title',
+                    createdAt: invalidCreatedAt,
+                })
+
+                expect(result.failed).toBeTruthy()
+            })
+
+            test.each([null, undefined, '', '7'])('Given lastModifiedAt is not a number', async (invalidLastModifiedAt) => {
+                const result = await AppCommands.saveNote('valid Id', {
+                    title: 'valid title',
+                    createdAt: 12345
+                    lastModifiedAt: invalidLastModifiedAt,
+                })
+
+                expect(result.failed).toBeTruthy()
+            })
+
+            test('Given AsyncStorage save fails', async () => {
+                AsyncStorage.setItem = jest.fn().mockRejectedValueOnce(new Error('Error setting item'))
+
+                await AppCommands.saveNote('any id', {})
+            })
         })
     })
 })
--- src/commands/AppCommands.ts
+++ src/commands/AppCommands.ts
@@ -1,3 +1,21 @@
-const saveNote = async () => {}
+const saveNote = async (id: string, content: { title: string; createdAt: number; lastModifiedAt: number }) => {
+    if (!id || id.trim().length === 0) {
+        return { failed: { reason: `Id ${id} is invalid` } }
+    }
+
+    const { title } = content
+    if (!title || title.trim().length === 0) {
+        return { failed: { reason: `Title ${title} is invalid` } }
+    }
+    const { createdAt } = content
+    if (!Number.isInteger(createdAt)) {
+        return { failed: { reason: `CreatedAt ${createdAt} is not a number` } }
+    }
+
+    const { lastModifiedAt } = content
+    if (!Number.isInteger(lastModifiedAt)) {
+        return { failed: { reason: `LastModifiedAt ${lastModifiedAt} is not a number` } }
+    }
+}
 
 export default { saveNote }
